28f448b76cea0d980f29afc8140ef62fb6984faf
gurami152 <gurami152@gmail.com>
2020-05-07 16:07:20 +0300

    Create full working app

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ require("firebase/database");
 
 var mountPoint = '#root';
 var articleID;
-
-
-
+var user=null;
+var userCreated=false;
+var CurrentEditingImgSrc;
 
 var firebaseConfig = {
   apiKey: "AIzaSyD6LMPI4pdqO92UJCay8cyYluGPIEaT1gU",
@@ -25,12 +25,76 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 $(document).ready(function () {
-  //Social and Anonymous Auth Scheme Login
-  $('#GoogleButton,#GithubButton,#anonymous').on('click', function(e) {
-    login($(this).attr('id'));
+  hashValue='listOfArticles';
+  $.get(hashValue+'.html', function(data){
+    $(mountPoint).empty().html(data);
+    $('#Login').on('click', function(e) {
+      AutorizationPageOpen();
+    })
+    $('#Logout').hide();
+    $('#Add').hide();
+    var query = firebase.database().ref("article").orderByKey('Date');
+    query.once("value")
+    .then(function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+        document.querySelector('#content')
+        .innerHTML += articleHtmlFromObject(childSnapshot.val(),childSnapshot.key);
+      });
+      articleClickSet();
+    });
+    
   })
-  
 });
+
+function AutorizationPageOpen(){
+  hashValue='autorizationPage';
+  $.get(hashValue+'.html', function(data){
+    $(mountPoint).empty().html(data);
+    $('#PasswordError').hide();
+    //Social and Anonymous Auth Scheme Login
+    $('#GoogleButton,#GithubButton,#LoginButton').on('click', function(e) {
+    login($(this).attr('id'));
+    })
+    $('#RegisterButton').on('click', function(e) {
+      RegisterPageOpen();
+    })
+    if(userCreated){
+      $('#UserCreatedMessage').show();
+    }
+    else{
+      $('#UserCreatedMessage').hide();
+    }
+  })  
+}
+
+function RegisterPageOpen(){
+  $.get('registerPage.html', function(data){
+    $(mountPoint).empty().html(data);
+    $('#Back').on('click', function(e) {
+      AutorizationPageOpen();
+    });
+    $('#UserRegisterButton').on('click', function(e){
+      var email = $('#email').val();
+      var password =$('#password').val();
+      var passwordConfirm =$('#passwordConfirm').val();
+      console.log(email);
+      console.log(password);
+      console.log(passwordConfirm);
+      if(password==passwordConfirm){
+        firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then( function(){
+          userCreated=true;
+          AutorizationPageOpen();
+        })
+        .catch(function(err){
+        console.error(err);
+        });
+      }
+    });
+  })
+}
 
 function AuthSuccesfull(){
   hashValue='listOfArticles';
@@ -42,6 +106,13 @@ function AuthSuccesfull(){
     $('#Add').on('click', function(e) {
       AddClick();
     })
+    if(user){
+      $('#Login').hide();
+    }
+    else{
+      $('#Logout').hide();
+      $('#Add').hide();
+    }
     var query = firebase.database().ref("article");
     query.once("value")
     .then(function(snapshot) {
@@ -57,8 +128,8 @@ function AuthSuccesfull(){
 }
 
 function articleClickSet(){
-  $('.article').on('click', function(e) {
-    articleID=this.id;
+  $('.article_image,.article_anotation').on('click', function(e) {
+    articleID=this.parentElement.id;
     $.get('detailArticle.html', function(data){
       $(mountPoint).empty().html(data);
       var articleRef = firebase.database().ref('article/' +articleID+'/');
@@ -71,8 +142,17 @@ function articleClickSet(){
         AuthSuccesfull();
       })
     })
-      
   })
+    $('.editButton').on('click', function(e) {
+      editClick(this.parentElement.id);
+    })
+    $('.deleteButton').on('click', function(e) {
+      firebase.database().ref("article").child(this.parentElement.id).remove()
+      .then(function(){
+        AuthSuccesfull();
+      })
+    })
+
 }
 
 function detailArticleHtml(article){
@@ -94,7 +174,30 @@ function articleHtmlFromObject(article,key){
       html += '<p>'+article.Date+'</p>';
       html += '<p>'+article.Anotation+'</p>';
     html += '</div>';
+    if(user){
+      html+='<div class="editButton"><img src=images/icon_edit.png>Редактировать</div>';
+      html+='<div class="deleteButton"><img src=images/icon_trash.png>Удалить</div>';
+    }
   html += '</div>';
+  return html;
+}
+
+function editArticleHtml(article){
+  var html = '';
+    html += '<h2>Изменить статью</h2>';
+    html += '<form>';
+      html += '<p>Заголовок статьи</p>';
+      html += '<textarea id="name">'+article.Name+'</textarea>';
+      html += '<p>Анотация к статье</p>';
+      html += '<textarea id="anotation">'+article.Anotation+'</textarea>';
+      html += '<p>Статья</p>';
+      html += '<textarea id="article">'+article.TextOfArticle+'</textarea>';
+      html += '<p>Текущее фото к статье</p>';
+      html += '<img src="'+article.ImageURL+'">';
+      html += '<p>Выберите другое если необходимо</p>';
+      html += '<input type="file" id="photo" accept=".jpg, .jpeg, .png">';
+    html += '</form>';
+    CurrentEditingImgSrc=article.ImageURL;
   return html;
 }
 
@@ -106,6 +209,25 @@ function AddClick(){
     var ReturnBackButton = document.getElementById("Back");
     ReturnBackButton.onclick=AuthSuccesfull;
   })
+}
+
+function editClick(articleID){
+  $.get('editArticle.html', function(data){
+    $(mountPoint).empty().html(data);
+    var articleRef = firebase.database().ref('article/' +articleID+'/');
+    console.log(articleID);
+    articleRef.on('value', function(snapshot) {
+      document.querySelector('#content')
+      .innerHTML += editArticleHtml(snapshot.val());
+    });
+    $('#editArticleButton').on('click',function(e){
+      editArticleFirebase(articleID);
+    });
+    $('#Back').on('click', function(e) {
+      AuthSuccesfull();
+    });
+  });
+  
 }
 
 function login (type) {
@@ -122,18 +244,21 @@ function login (type) {
       break;
     }
 
-    case 'anonymous': {
-      request = firebase.auth().signInAnonymously();
-      if( request !== null ){
-        return request
-        .then(function (user) {
-          console.log(user);
-          AuthSuccesfull();
-          return user;
-        })
-      }
+    case 'LoginButton': {
+      var email = $('#email').val();
+      var password =$('#password').val();
+      firebase.auth().signInWithEmailAndPassword(email, password)
+      .then( function(result) {
+        user = result.user;
+        AuthSuccesfull();
+      })
+      .catch(function(err){
+        $('#PasswordError').show();
+        console.error(err);
+      });
       break;
     }
+    
   }
 }
 
@@ -142,7 +267,7 @@ function Auth(provider){
       .auth()
       .signInWithPopup(provider)
       .then(function(result) {
-        var user = result.user;
+        user = result.user;
         AuthSuccesfull();
         console.log(user)
       })
@@ -156,6 +281,7 @@ function Logout(){
     .auth()
     .signOut()
     .then(function () {
+        user=null;
         console.log("True");
         location.reload();
         return true; //Do necessary cleanup
@@ -197,7 +323,45 @@ function LoadDataToDateBase(ImageURL){
     };
     var updates = {};
     updates['/article/' + newArticleKey] = now;
-    return firebase.database().ref().update(updates);
+    return firebase.database().ref().update(updates).then(function(){
+      AuthSuccesfull();
+    });
+    } else {
+      alert('Пожалуйста введите заголовок и статью');
+    }
+}
+
+function editArticleFirebase(articleID){
+  var FileName = $('#photo').val();
+  if(FileName!=''){
+    const ref = firebase.storage().ref();
+    const file = document.querySelector('#photo').files[0]
+    const name = (+new Date()) + '-' + file.name;
+    const metadata = {
+    contentType: file.type
+    };
+    const task = ref.child(name).put(file, metadata);
+    task
+    .then(snapshot => snapshot.ref.getDownloadURL())
+    .then((url) => {
+      CurrentEditingImgSrc=url;
+    })
+    .catch(console.error);
+  }
+  console.log("OK");
+  if( document.querySelector('#name').value != '' || document.querySelector('#article').value != '' ){
+    var now = {
+      Name: document.querySelector('#name').value,
+      TextOfArticle: document.querySelector('#article').value,
+      Date: new Date(),
+      ImageURL:CurrentEditingImgSrc,
+      Anotation: document.querySelector('#anotation').value
+    };
+    var updates = {};
+    updates['/article/' +articleID] = now;
+    return firebase.database().ref().update(updates).then(function(){
+      AuthSuccesfull();
+    });
     } else {
       alert('Пожалуйста введите заголовок и статью');
     }
